0c8393c2044f561039fc26acf486957c94a7e851
Rishabh Parsediya <parsediyarishabh@gmail.com>
2025-07-29 14:08:27 +0530

    feat: payment and order routes updated

--- order-service/routes/orderRoutes.js
+++ order-service/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { placeOrder, getOrderById, getOrdersByUserId } = require('../controllers/orderController');
+const { placeOrder, getOrderById, getOrdersByUserId, updateStatusOfOrder } = require('../controllers/orderController');
 
 router.post('/place', placeOrder);
 router.get('/:orderId', getOrderById);
 router.get('/user/:userId', getOrdersByUserId);
+router.post('/update-order-status', updateStatusOfOrder);
 
 module.exports = router;
--- payment-service/routes/paymentRoutes.js
+++ payment-service/routes/paymentRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { processPayment } = require('../controllers/paymentController');
+const { processPayment, updatePaymentStatus } = require('../controllers/paymentController');
 
 router.post('/create-checkout-session', processPayment);
+router.post('/update-payment-status', updatePaymentStatus);
 
 module.exports = router;
